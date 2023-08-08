f7e2d9a20bdb66931a587f0fc1c66ed0feb880cb
Ryan Rigor <ryanrigor4@gmail.com>
2023-08-08 14:35:04 -0700

    Create and Close connection to DB each request

--- main.py
+++ main.py
@@ -16,10 +16,6 @@ redirect text
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-#creates table and establishes connection
-conn = createConnection(dbDirectory)
-createTable(conn, requestTable)
-
 #request body
 class url(BaseModel):
     original: str = ""
@@ -29,32 +25,42 @@ app = FastAPI()
 
 @app.delete("/deleteall")
 async def deleteAll():
+    conn = createConnection(dbDirectory)
     query = f"DELETE FROM url"
     deleteLinks(conn, query)
+    conn.close()
 
 @app.delete("/deleteLink/{key}")
 async def deleteRedirect(key: str):
+    conn = createConnection(dbDirectory)
     query = f"DELETE FROM URL WHERE redirect=(?)"
     deleteLink(conn, query, key)
+    conn.close()
     return key
 
 @app.post("/createLink/")
 async def create_Link(url: url):
+    conn = createConnection(dbDirectory)
     print(url.original)
     id = id_generator()
     insertion = f"INSERT INTO url VALUES('{url.original}','{id}')"
     insertLink(conn, insertion)
+    conn.close()
     return id
 
 @app.get("/getLinks")
 async def get_Link():
+    conn = createConnection(dbDirectory)
     query = "SELECT * from url"
     console = getLinks(conn, query)
+    conn.close()
     return console
 
 @app.get("/{key}")
 async def retrieveLink(key: str):
+    conn = createConnection(dbDirectory)
     query = f"SELECT * from url WHERE redirect=(?)"
     print(query)
     op = fetchRedirect(conn, query, key)
+    conn.close()
     return RedirectResponse(url=op[0][0]) 
